340f1c295bab42366c1cdd6ba7b576a104caa81f
Mihail R <88396616+MikeReCode@users.noreply.github.com>
2022-03-26 21:11:57 +0200

    Update settings.py

--- Online_Image_Gallery/Online_Image_Gallery/settings.py
+++ Online_Image_Gallery/Online_Image_Gallery/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)_=-tnr5_#&0+g*xjo$!6$xtrv5e%gkl7j^lf#07@_%obe$kz#'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
