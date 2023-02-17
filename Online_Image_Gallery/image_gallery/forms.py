cae72f02024fcf050bd6e50685cb0d43303430da
MikeReCode <mihail.radu.work@gmail.com>
2023-02-18 00:29:21 +0200

    Add style to add image view

--- Online_Image_Gallery/image_gallery/forms.py
+++ Online_Image_Gallery/image_gallery/forms.py
@@ -3,7 +3,11 @@ from image_gallery.models import Image
 
 
 class ImageForm(forms.ModelForm):
-    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'id': 'custom-file-input'}))
+
+    def __init__(self, *args, **kwargs):
+        super(ImageForm, self).__init__(*args, **kwargs)
+        self.fields['image'].label = ''
 
     class Meta:
         model = Image
